1a8e7f47341f15c4aebc67095056b26c687eec54
BooDevSecOps <boo.fi2023@gmail.com>
2024-07-08 01:17:58 +0800

    Update servers.component.spec.ts

--- src/app/views/servers/servers.component.spec.ts
+++ src/app/views/servers/servers.component.spec.ts
@@ -4,7 +4,7 @@ import { CardModule, GridModule } from '@coreui/angular';
 import { ChartjsModule } from '@coreui/angular-chartjs';
 import { IconSetService } from '@coreui/icons-angular';
 import { iconSubset } from '../../icons/icon-subset';
-import { ServersComponent } from './servers.component';
+import { ChartsComponent } from './charts.component';
 
 describe('ServersComponent', () => {
   let component: ServersComponent;
