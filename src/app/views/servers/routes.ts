686d7fd023304913f5eea5fd91ac2b358aaa03cc
BooDevSecOps <boo.fi2023@gmail.com>
2024-07-08 01:19:16 +0800

    Update routes.ts

--- src/app/views/servers/routes.ts
+++ src/app/views/servers/routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { ServersComponent } from './servers.component';
+import { ChartsComponent } from './charts.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ServersComponent,
+    component: ChartsComponent,
     data: {
       title: 'Servers'
     }
